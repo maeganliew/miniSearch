361ac6e8763dc01d930a6eb246438dc810134d75
Liew Jia Wei <mjw.liew@gmail.com>
2026-01-01 01:33:56 +0800

    fix

--- src/main/java/com/minisearch/MiniSearchApplication.java
+++ src/main/java/com/minisearch/MiniSearchApplication.java
@@ -3,7 +3,7 @@ package com.minisearch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+@SpringBootApplication
 public class MiniSearchApplication {
 
     public static void main(String[] args) {
